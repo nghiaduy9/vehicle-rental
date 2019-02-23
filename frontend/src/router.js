a384891c878947d2d46c26d4489f4a9325e6846a
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-02-24 01:40:41 +0700

    Build basic structure of the app

---
+++ frontend/src/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Index from './views/index.vue'
+import Login from './views/login.vue'
+import Dashboard from './views/dashboard.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    }
+  ]
+})
---
+++ frontend/src/views/dashboard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This is the dashboard page</h1>
+  </div>
+</template>
